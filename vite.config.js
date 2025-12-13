b62fe0a11850ad38c2198383413a5b008103f02b
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2025-12-13 11:38:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: "medi-time ai",
-        short_name: "medi-time",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#0f172a",
-        theme_color: "#0f172a",
-        icons: [
-          {
-            src: "/icons/icon-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icons/icon-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      },
-      registerType: 'autoUpdate'
-    })
-  ]
+  plugins: [react()],
 })
