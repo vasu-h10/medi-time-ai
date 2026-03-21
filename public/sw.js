dde217f1cd78b5b847f8f318c1dba19627687684
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2026-03-21 16:34:57 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  // basic fetch
+});
