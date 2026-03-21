1133eafafd40b08ff5e5dd7da0384b5db64c0b4e
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2026-03-21 16:51:20 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,33 @@
+const CACHE_NAME = "medi-time-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activate
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated");
 });
 
+// Fetch (offline support)
 self.addEventListener("fetch", (event) => {
-  // basic fetch
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
